3a46aa9b769c14320d75e2e6de42460619e75ed1
orangethemeow <81650558+orangethemeow@users.noreply.github.com>
2024-01-03 20:23:38 -0500

    Update observe_state.py

--- 2_state_preparation/cirq/02_observe_state/observe_state.py
+++ 2_state_preparation/cirq/02_observe_state/observe_state.py
@@ -1,1 +1,13 @@
+from math import atan2
+import cirq
 
+alpha, beta = 0.6, 0.8
+theta = atan2(beta, alpha)
+
+qc = cirq.Circuit()
+qc.append(cirq.Ry(rads=2 * theta)(*cirq.LineQubit.range(1)))
+simulator = cirq.Simulator()
+res = simulator.simulate(qc)
+
+state_vector = res.final_state_vector
+print(state_vector)
