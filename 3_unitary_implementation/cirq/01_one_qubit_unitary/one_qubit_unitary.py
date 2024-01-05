722dacf8eeef57146e6aa432b8526a5e1bd2442c
orangethemeow <81650558+orangethemeow@users.noreply.github.com>
2024-01-04 21:00:11 -0500

    Update one_qubit_unitary.py

--- 3_unitary_implementation/cirq/01_one_qubit_unitary/one_qubit_unitary.py
+++ 3_unitary_implementation/cirq/01_one_qubit_unitary/one_qubit_unitary.py
@@ -1,1 +1,19 @@
+import numpy as np
+from math importisclose
+import cirq
 
+class OneQubitUnitary(cirq.Gate):
+    def __init__(self, u):
+        super(OneQubitUnitary, self)
+        self.u = u
+        
+    def _num_qubits_(self):
+        return 1
+    
+    def _unitary_(self):
+        if isclose(self.u[0][0], -self.u[1][1]) and isclose(self.u[1][0], self.u[0][1]):
+            return np.matmul(np.array([[1, 0], [0, -1]]), self.u)       
+        return np.array(self.u)
+    
+    def _circuit_diagram_info_(self, args):
+        return f"apply one qubit({self.u})" 
