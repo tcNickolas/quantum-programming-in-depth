3eade4417fff2708abf134a6bfd9d2a12e59b4f1
orangethemeow <81650558+orangethemeow@users.noreply.github.com>
2024-01-11 18:16:15 -0500

    Update two_qubit_cs_matrix_using_prev_gate.py

--- 3_unitary_implementation/cirq/    06_two_qubit_cs_matrix  /unitaryimplementation/two_qubit_cs_matrix_using_prev_gate.py
+++ 3_unitary_implementation/cirq/    06_two_qubit_cs_matrix  /unitaryimplementation/two_qubit_cs_matrix_using_prev_gate.py
@@ -1,1 +1,43 @@
+import numpy as np
+from math import isclose
+import cirq
 
+class OneQubitUnitary(cirq.Gate):
+    def __init__(self, u):
+        super(OneQubitUnitary, self)
+        self.u = u
+        
+    def _num_qubits_(self):
+        return 1
+    
+    def _unitary_(self):
+        if isclose(self.u[0][0], -self.u[1][1]) and isclose(self.u[1][0], self.u[0][1]):
+            return np.matmul(np.array([[1, 0], [0, -1]]), self.u)       
+        return np.array(self.u)
+    
+    def _circuit_diagram_info_(self, args):
+        return f"apply one qubit({self.u})" 
+
+
+class CsMatrix_2(cirq.Gate):
+    def __init__(self, c0, s0, c1, s1):
+        super(CsMatrix_2, self)
+        self.c0 = c0
+        self.s0 = s0
+        self.c1 = c1
+        self.s1 = s1
+        self.gate_m0 = OneQubitUnitary([[c0, -s0], [s0, c0]])
+        self.gate_m1 = OneQubitUnitary([[c1, -s1], [s1, c1]])
+    
+    def _num_qubits_(self):
+        return 2
+    
+    def _decompose_(self, qubits):
+        control_1 = self.gate_m1.controlled().on(qubits[1], qubits[0])
+        control_2 = self.gate_m0.controlled(control_values=[0]).on(qubits[1], qubits[0])
+        
+        yield control_1
+        yield control_2 
+        
+    def _circuit_diagram_info_(self, args):
+        return f"cs matrix with({self.c0, self.s0, self.c1, self.s1})"
