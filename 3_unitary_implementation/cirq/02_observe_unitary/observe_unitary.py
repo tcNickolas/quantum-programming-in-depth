4ca0e4f5bdabf502fab74999d209b58f561c77a1
orangethemeow <81650558+orangethemeow@users.noreply.github.com>
2024-01-13 13:19:04 -0500

    Update observe_unitary.py

--- 3_unitary_implementation/cirq/02_observe_unitary/observe_unitary.py
+++ 3_unitary_implementation/cirq/02_observe_unitary/observe_unitary.py
@@ -1,1 +1,20 @@
-# not sure how to show gate (on more than one qubit) in matrix form in cirq
+import numpy as np
+from math importisclose
+import cirq
+
+class OneQubitUnitary(cirq.Gate):
+    def __init__(self, u):
+        super(OneQubitUnitary, self)
+        self.u = u
+        
+    def _num_qubits_(self):
+        return 1
+    
+    def _unitary_(self):      
+        return np.array(self.u)
+    
+    def _circuit_diagram_info_(self, args):
+        return f"apply one qubit({self.u})" 
+
+coef = [[0.6, -0.8], [0.8, 0.6]]
+print(cirq.unitary(OneQubitUnitary(coef)))
