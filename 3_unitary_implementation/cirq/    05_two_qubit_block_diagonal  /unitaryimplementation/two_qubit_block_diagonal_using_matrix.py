50f1ddf000dece8a9308a257cc6930fe0c7db7ff
orangethemeow <81650558+orangethemeow@users.noreply.github.com>
2024-01-13 13:26:23 -0500

    Update two_qubit_block_diagonal_using_matrix.py

--- 3_unitary_implementation/cirq/    05_two_qubit_block_diagonal  /unitaryimplementation/two_qubit_block_diagonal_using_matrix.py
+++ 3_unitary_implementation/cirq/    05_two_qubit_block_diagonal  /unitaryimplementation/two_qubit_block_diagonal_using_matrix.py
@@ -12,11 +12,6 @@ class BlockDiagonalMatrix(cirq.Gate):
         return 2
 
     def _unitary_(self):     
-        if isclose(a[0][0], -a[1][1]) and isclose(a[1][0], a[0][1]):
-            self.a = np.matmul(np.array([[1, 0], [0, -1]]), self.a)  
-        if isclose(b[0][0], -b[1][1]) and isclose(b[1][0], b[0][1]):
-            self.b = np.matmul(np.array([[1, 0], [0, -1]]), self.b) 
-    
         return np.array([
             [self.a[0][0], self.a[0][1], 0, 0], [self.a[1][0], self.a[1][1], 0, 0], 
             [0, 0, self.b[0][0], self.b[0][1]], [0, 0, self.b[1][0], self.b[1][1]]])
