5cf6c5e2ec50da67df2938dfbb063a525235a4f3
orangethemeow <81650558+orangethemeow@users.noreply.github.com>
2024-01-13 11:57:13 -0500

    Update two_qubit_block_diagonal_using_prev_gate.py

--- 3_unitary_implementation/cirq/    05_two_qubit_block_diagonal  /unitaryimplementation/two_qubit_block_diagonal_using_prev_gate.py
+++ 3_unitary_implementation/cirq/    05_two_qubit_block_diagonal  /unitaryimplementation/two_qubit_block_diagonal_using_prev_gate.py
@@ -10,9 +10,7 @@ class OneQubitUnitary(cirq.Gate):
     def _num_qubits_(self):
         return 1
     
-    def _unitary_(self):
-        if isclose(self.u[0][0], -self.u[1][1]) and isclose(self.u[1][0], self.u[0][1]):
-            return np.matmul(np.array([[1, 0], [0, -1]]), self.u)       
+    def _unitary_(self):     
         return np.array(self.u)
     
     def _circuit_diagram_info_(self, args):
