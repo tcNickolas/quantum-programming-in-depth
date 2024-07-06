05fb283fddaf4c6cb89807cc56da4802609de203
Mariia Mykhailova <michaylova@gmail.com>
2024-07-06 09:01:04 -0700

    Remove code for 6.6

--- 6_reversible_computing/qsharp_v1/06_integer_addition/test_integer_addition.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from qsharp import init, eval
-
-@pytest.mark.parametrize("n", [1, 2, 3, 4, 5])
-def test_adder(n):
-  init(project_root='.')
-  eval(f"ReversibleComputing.Test.TestAdder({n})")
