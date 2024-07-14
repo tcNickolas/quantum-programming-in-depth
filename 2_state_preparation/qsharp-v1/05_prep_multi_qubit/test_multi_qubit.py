1f6c34903b392de49ce7974163059601f570e622
Mariia Mykhailova <michaylova@gmail.com>
2024-07-14 08:28:54 -0700

    Update tests to AQDK with global phase issue fixed

--- 2_state_preparation/qsharp-v1/05_prep_multi_qubit/test_multi_qubit.py
+++ 2_state_preparation/qsharp-v1/05_prep_multi_qubit/test_multi_qubit.py
@@ -7,19 +7,7 @@ def run_test_prep_multi_qubit(n, a):
   qsharp.init(project_root='.')
   qsharp.eval(f"use qs = Qubit[{n}]; StatePreparation.PrepArbitrary(qs, {a});")
   state = qsharp.dump_machine()
-
-  # Find global phase difference
-  global_phase = -2
-  for ind in range(len(a)):
-    if abs(a[ind]) > 1E-9:
-        global_phase = state[ind] / a[ind]
-        break
-
-  for ind in range(len(a)):
-    if abs(a[ind]) > 1E-9:
-      assert state[ind] == pytest.approx(a[ind] * global_phase)
-    else:
-      assert ind not in state
+  assert state.check_eq(a)
 
 
 def test_basis_states():
