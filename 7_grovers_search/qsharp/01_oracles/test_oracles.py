763cd254a7b350838288267800f2e387addbeecc
Edoardo Altamura <38359901+edoaltamura@users.noreply.github.com>
2024-10-20 20:27:56 +0200

    Fix ch7 oracle qs test

--- 7_grovers_search/qsharp/01_oracles/test_oracles.py
+++ 7_grovers_search/qsharp/01_oracles/test_oracles.py
@@ -21,15 +21,15 @@ def test_mark_states(n, markedStates):
        f"GroversSearch.Test.FMarkStates(_, {markedStates}))")
 
 
-@pytest.mark.parametrize("n, marked_states", test_cases)
-def test_apply_phase_oracle(n, marked_states):
+@pytest.mark.parametrize("n, markedStates", test_cases)
+def test_apply_phase_oracle(n, markedStates):
   init(project_root='.')
   matrix = dump_operation(f"GroversSearch.ApplyPhaseOracle(_, GroversSearch.MarkStates(_, _, {markedStates}))", n)
 
   complete_coef = []
   for state in range(2 ** n):
     row = [0] * (2 ** n)
-    row[state] = -1 if state in marked_states else 1
+    row[state] = -1 if state in markedStates else 1
     complete_coef += [row]
 
   for actual, expected in zip(matrix, complete_coef):
